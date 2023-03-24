0ebc5cea4b4605d84880bb1a5151f4ed5cf02335
TakanaQU <sthsth.u.negi@gmail.com>
2023-03-24 17:20:56 +0900

    Initial Commit

---
+++ Diamemory/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Diamemory
+//
+//  Created by たな on 2023/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Diamemory/DiamemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiamemoryApp.swift
+//  Diamemory
+//
+//  Created by たな on 2023/03/24.
+//
+
+import SwiftUI
+
+@main
+struct DiamemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
