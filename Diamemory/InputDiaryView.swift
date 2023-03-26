2b3e013e5c9d01cee9856b3c8bae6f0ba75ed478
TakanaQU <sthsth.u.negi@gmail.com>
2023-03-27 00:08:25 +0900

    Add InputDiary View

---
+++ Diamemory/InputDiaryView.swift
@@ -0,0 +1,20 @@
+//
+//  InputDiaryView.swift
+//  Diamemory
+//
+//  Created by たな on 2023/03/27.
+//
+
+import SwiftUI
+
+struct InputDiaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InputDiaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        InputDiaryView()
+    }
+}
