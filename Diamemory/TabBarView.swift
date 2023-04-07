c30abb96dc8369f794b8828c1de7fb2e16158207
TakanaQU <sthsth.u.negi@gmail.com>
2023-04-07 21:12:02 +0900

    change TAbBar color

--- Diamemory/TabBarView.swift
+++ Diamemory/TabBarView.swift
@@ -13,19 +13,23 @@ struct TabBarView: View {
         TabView{
             RecordButton(isRecording: false)
                 .tabItem {
-                    Image(systemName: "pencil.line")
+                    Image(systemName: "pencil")
+                    Text("日記をつける")
                 }
             TaskView()
                 .tabItem{
                     Image(systemName: "list.bullet")
+                    Text("タスク")
                 }
             PastDiaryView()
                 .tabItem {
                     Image(systemName: "book.fill")
-                    
+                    Text("日記帳")
                 }
-            
         }
+        //Color("PaleBlue")
+        .accentColor(Color("DarkBlue"))
+        //.foregroundColor(Color.brown) Text in all view get brown
     }
 }
 
